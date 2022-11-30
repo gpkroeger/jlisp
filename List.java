235ddfd8ab46eae38a5d8250c377148277dffc4b
Gabe Kroeger <gpkroeger@gmail.com>
2022-11-30 15:38:56 -0600

    fixed bug where last element of list wasn't picked up

--- List.java
+++ List.java
@@ -28,7 +28,7 @@ public class List extends Expr {
     public boolean isList() {
         return true;
     }
-    
+
     public Expr getLeft() {
         return left;
     }
@@ -45,22 +45,23 @@ public class List extends Expr {
         return String.format("Type: %s, Left: %s, Others: %s", "LIST", left, others);
     }
 
-    //prereq: the parameter arraylist is between one open paren and one
-    //closed paren which are not present wihtin the indices at the time of passing through
+    // prereq: the parameter arraylist is between one open paren and one
+    // closed paren which are not present wihtin the indices at the time of passing
+    // through
     public static List listify(ArrayList<Expr> t, int left, int right) throws Exception {
         Expr l;
         ArrayList<Expr> others = new ArrayList<Expr>(0);
         Expr cur;
-        if(left == right) {
+        if (left == right) {
             return new List();
         }
 
         l = t.get(left++);
 
-        while(left < right) {
-            if(t.get(left).getType() == TokType.OPEN_PAREN) {
+        while (left <= right) {
+            if (t.get(left).getType() == TokType.OPEN_PAREN) {
                 int closeIndex = findCloser(t, left, right);
-                cur = listify(t, left+1, closeIndex-1);
+                cur = listify(t, left + 1, closeIndex - 1);
                 left = closeIndex;
             } else {
                 cur = t.get(left);
@@ -72,25 +73,27 @@ public class List extends Expr {
         return new List(l, others);
     }
 
-    //this is called with the first index as an open paren and by the closing index should contain the
-    //closing parenthesis, otherwise raises an exception
-    //returns the index of the matching closing parenthesis
+    // this is called with the first index as an open paren and by the closing index
+    // should contain the
+    // closing parenthesis, otherwise raises an exception
+    // returns the index of the matching closing parenthesis
     public static int findCloser(ArrayList<Expr> t, int left, int right) throws Exception {
         int open = 0;
         int close = 0;
-        while(true) {
-            if(left > right) {
+        while (true) {
+            if (left >= right) {
                 throw new Exception("Mismatched parenthesis");
             }
 
-            if(t.get(left).getType() == TokType.OPEN_PAREN) {
+            if (t.get(left).getType() == TokType.OPEN_PAREN) {
                 open++;
             }
-            if(t.get(left).getType() == TokType.CLOSE_PAREN) {
+            if (t.get(left).getType() == TokType.CLOSE_PAREN) {
                 close++;
             }
 
-            if(open == close) break;
+            if (open == close)
+                break;
             left++;
         }
 
